95cfcf3eeca48a748bcc20adcf7aadcd80320ec5
Alexander Huber <alih83@gmx.de>
2014-03-13 18:17:33 +0100

    model globbing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails4JqueryUpload::Engine.routes.draw do
   post   "uploads",            to: "uploads#create"
-  delete "uploads/:model/:id", to: "uploads#destroy"
+  delete "uploads/*model/:id", to: "uploads#destroy"
   patch  "uploads",            to: "uploads#create"
 end
